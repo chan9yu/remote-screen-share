12ae3265b79adc652ccbd8eaa5fb4a354a056a7d
chan9yu <dev.cgyeo@gmail.com>
2025-02-10 23:31:09 +0900

    chore: pretendard 폰트 추가, 설정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,15 @@
 export default function RootPage() {
 	return (
 		<div className="w-screen h-screen flex flex-col items-center justify-center gap-4 font-pretendard">
-			Hello, World!
+			<span className="font-thin">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-extralight">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-light">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-normal">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-medium">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-semibold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-bold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-extrabold">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
+			<span className="font-black">타이포그래피: ‘Typography’, 1972. @ㅇ0ㅇ ₩0 〈응♥〉 『힣♪』</span>
 		</div>
 	);
 }
